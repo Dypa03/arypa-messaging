a755ee9fd07b47af7cbec6033ae95429c2830108
Dypa03 <davidedipa03@gmail.com>
2026-04-27 13:50:43 +0200

    Folder structure setup

---
+++ message-service/src/test/java/com/message_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
