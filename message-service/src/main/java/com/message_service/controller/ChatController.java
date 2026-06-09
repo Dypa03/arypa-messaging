36c4564ac1e59b4f60f25120b0f1ecb4451ddd38
Dypa03 <davidedipa03@gmail.com>
2026-06-09 15:47:29 +0200

    Basic WebSocket implementation

---
+++ message-service/src/main/java/com/message_service/controller/ChatController.java
@@ -0,0 +1,32 @@
+package com.message_service.controller;
+
+import com.message_service.model.dto.MessageDTO;
+import com.message_service.model.entity.Message;
+import com.message_service.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void sendMessage(MessageDTO messageDTO) {
+        Message message = new Message();
+        message.setContent(messageDTO.getContent());
+        message.setSenderId(messageDTO.getSenderId());
+        message.setChatId(messageDTO.getChatId());
+
+        messageRepository.save(message);
+
+        messagingTemplate.convertAndSend("/topic/messages/" + message.getChatId(), message);
+
+
+    }
+}
---
+++ message-service/src/main/resources/static/app.js
@@ -0,0 +1,35 @@
+var stompClient = null;
+
+var username = generateRandomUsername();
+
+function generateRandomUsername() {
+    var adjectives = ["Quick", "Lazy", "Happy", "Sad", "Angry"];
+    var nouns = ["Fox", "Dog", "Cat", "Mouse", "Bear"];
+    var adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    var noun = nouns[Math.floor(Math.random() * nouns.length)];
+    return adjective + noun + Math.floor(Math.random() * 1000);
+}
+
+function connect() {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/messages/5', function (message) {
+            showMessage(JSON.parse(message.body));
+        });
+    });
+}
+
+function sendMessage() {
+    var messageContent = document.getElementById('message-input').value;
+    stompClient.send("/app/chat", {}, JSON.stringify({'content': messageContent, 'senderId': 1, 'chatId': 5, 'username': username}));
+    document.getElementById('message-input').value = ''
+}
+
+function showMessage(message) {
+    var messagesDiv = document.getElementById('messages');
+    var messageElement = document.createElement('div');
+    messageElement.appendChild(document.createTextNode(message.content));
+    messagesDiv.appendChild(messageElement);
+}
