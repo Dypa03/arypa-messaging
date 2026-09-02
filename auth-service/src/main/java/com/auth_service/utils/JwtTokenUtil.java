996d8c8680c80d11dedd1a830dc8b2e1b4678597
Dypa03 <davidedipa03@gmail.com>
2026-09-02 14:00:45 +0200

    Started to implement JWT authentication on auth-service

--- auth-service/src/main/java/com/auth_service/utils/JwtTokenUtil.java
+++ auth-service/src/main/java/com/auth_service/utils/JwtTokenUtil.java
@@ -1,4 +1,18 @@
 package com.auth_service.utils;
 
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
 public class JwtTokenUtil {
+    Map<String, String> env = System.getenv();
+
+    private String SECRET_KEY = env.get("SECRET_KEY");
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    
 }
